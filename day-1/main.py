307a0b7da4a0a70289d54ed7a16501edb896c10a
Sean Barry <se@nbarry.co.uk>
2022-12-01 17:59:51 +0000

    part two and part 1 refactor

--- day-1/main.py
+++ day-1/main.py
@@ -1,19 +1,34 @@
+def evaluate_calorie_count(count, top3):
+    if count < top3[2]:
+        return
+    elif count > top3[0]:
+        top3[2], top3[1], top3[0] = top3[1], top3[0], count
+    elif count > top3[1]:
+        top3[2], top3[1] = top3[1], count
+    elif count > top3[2]:
+        top3[2] = count
+
+def part_two(data):
+    top_three = [float('-inf') for i in range(3)]
+    
+    current_total = 0
+    for line in data:
+        if line == '\n':
+            evaluate_calorie_count(current_total, top_three)
+            current_total = 0
+            continue
+
+        current_total += int(line.strip())
+            
+    return top_three
+
+
 if __name__ == '__main__':
     with open('./data.txt') as data:
         data =  data.readlines()
+        
+        top3 = part_two(data)
 
-        max_calories = float('-inf')
-        current_total = 0
+        print(f"Part One Answer: {top3[0]}")
+        print(f"Part Two Answer: {sum(top3)}")
 
-        for line in data:
-            if line == '\n':
-                max_calories = max(current_total, max_calories)
-                current_total = 0
-                continue
-
-            current_total += int(line.strip())
-
-        print('max', max_calories)
-
-
-            
