44f21df20529cc73fe3f7ba60cdfa8112d8e3249
Sean Barry <se@nbarry.co.uk>
2022-12-01 18:05:47 +0000

    typing and tidying up

--- day-1/main.py
+++ day-1/main.py
@@ -1,4 +1,6 @@
-def evaluate_calorie_count(count, top3):
+from typing import List
+
+def evaluate_calorie_count(count: int, top3: List[int]):
     if count <= top3[2]:
         return
     elif count > top3[0]:
@@ -8,7 +10,7 @@ def evaluate_calorie_count(count, top3):
     elif count > top3[2]:
         top3[2] = count
 
-def part_two(data):
+def find_top_three_calorie_counts(data: List[str]) -> List[str]:
     top_three = [float('-inf') for i in range(3)]
     
     current_total = 0
@@ -27,7 +29,7 @@ if __name__ == '__main__':
     with open('./data.txt') as data:
         data =  data.readlines()
         
-        top3 = part_two(data)
+        top3 = find_top_three_calorie_counts(data)
 
         print(f"Part One Answer: {top3[0]}")
         print(f"Part Two Answer: {sum(top3)}")
