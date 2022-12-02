f53a15a0f9bf2c7a3d3977abf1ccbb62482e1054
Sean Barry <se@nbarry.co.uk>
2022-12-02 22:25:42 +0000

    day 2 part 2

--- day-2/main.py
+++ day-2/main.py
@@ -1,18 +1,36 @@
-if __name__ == '__main__':
+def part_one(data):
+    score = 0
+
     wins = {'X':'C','Y':'A','Z': 'B'}
     draws = {'X':'A','Y':'B','Z':'C'}
+    # x lose, y draw, z win
+    for game in data:
+        them, us = game.split()
+        score += list(wins.keys()).index(us) + 1
 
-    with open('./data.txt') as data:
-        score = 0
+        if wins[us] == them:
+            score += 6
+        elif draws[us] == them:
+            score += 3
+    return score
 
-        for game in [line.rstrip().split(' ') for line in data]:
-            them, us = game
-            score += list(wins.keys()).index(us) + 1
-
-            if wins[us] == them:
-                score += 6
-            elif draws[us] == them:
-                score += 3
-
-        print(f'Total Score: {score}')
         
+def part_two(data):
+    score = 0
+    mapping = {"AX":3,"AY":4,"AZ":8,"BX":1,"BY":5,"BZ":9,"CX":2,"CY":6,"CZ":7}
+    
+    for game in data:
+        fixture = game.replace(' ', '')
+
+        score += mapping[fixture]
+    return score
+
+if __name__ == '__main__':
+    with open('./data.txt') as data:
+        rounds = [line.rstrip() for line in data]
+        one = part_one(rounds)
+        two = part_two(rounds)
+
+        print(f'Score for ONE is: {one}')
+        print(f'Score for TWO is: {two}')
+
